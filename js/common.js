ead6e9aa8cda799038b444cc8a7fb47b57ac737f
SeeUzq <875343519@qq.com>
2020-11-22 21:03:59 +0800

    公共结构和banner图

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $('.header').load('./common_header.html');
 
 $('.footer').load('./common_footer.html');
-var timer = setInterval(() => {
+var timer = setInterval(function(){
    
     $('.commonHeader_top_inner .notice li').animate({
         top:-30
@@ -14,30 +14,58 @@ var timer = setInterval(() => {
 
 // 网易优选接口
 
-   setTimeout(() => {
+   setTimeout(function(){
     $.ajax({
         url:'./../data/data.json',
         type:'get',
         dataType:'json',
         success:function(data){
-           console.log(data)
            var jsonData = data[0].data.cateList
-           console.log(jsonData)
            var i = 0
-           for (const key in jsonData) {
+    
+           for (var  key in jsonData) {
                i++
                if (jsonData.hasOwnProperty(key)) {
-                   const element = jsonData[key];
-                   console.log(element.id,element.name)
-                   var str ="<a href='#'  class='liA' index="+ i +" id="+element.id+">"+element.name+"</a>"
-                   $('.main_nav .main_nav_ul').append("<li>"+str+"</li>")
-                   console.log($('.main_nav .main_nav_ul'))
+                   var  element = jsonData[key];
+                //    console.log(element)//element是获得每一个导航栏的值li下每一页的
+                   var strA ="<a href='#'  class='liA' index="+ i +" id="+element.id+">"+element.name+"</a>"
+                 
+                   var li = $("<li class ='morePage clearfix lis_"+i+"'>"+strA+"</li>")
+                   $('.main_nav .main_nav_ul').append(li)
+                    var subCateGroupList = element.subCateGroupList
+                    var listDiv = $('<div class="list_items clearfix"><div>')
+
+                    for (var valKey in subCateGroupList) {//val是每一页中的一列
+                        if (subCateGroupList.hasOwnProperty(valKey)) {
+                            var values = subCateGroupList[valKey];
+                            var categoryList =values.categoryList
+                            var strAll = "<dt>"+values.name+"</dt>"
+                            for (var sampleKey in categoryList) {//sample是每一列的每一各商品
+                                if (categoryList.hasOwnProperty(sampleKey)) {
+                                    var samples = categoryList[sampleKey];
+                                //    console.log(samples)
+                                   var str = "<dd id='"+samples.id+"'><a href='#'><img src='"+samples.bannerUrl+"'title = '"+samples.frontName+"'><span >"+samples.name+"</a></dd>"
+                                    strAll +=str
+                                }
+                            }
+                           var dl = $("<dl class='list_items_dl'>"+strAll+"</dl>")
+
+                           $(listDiv).append(dl)
+                        }
+                    }
+                    $(li).append(listDiv)
+
+
                  }
                }
+
+               var lastLis = $("<li class ='morePage  lis_yx'> <a href='#'  class='liA liA_last'>为你严选</a></li><li class ='morePage clearfix lis_zc'> <a href='#'  class='liA liA_last'>众筹</a></li>")
+               $('.main_nav .main_nav_ul').append(lastLis)
            }
- 
+           
  
          
     })
  
-   }, 2000);
+   }, 0);
+
---
+++ js/index.js
@@ -0,0 +1,1 @@
+$('.indexMain').load('./indexMain.html');
