301f87cce0bbb6de0ba147c38c2e6687d940aca8
SeeUzq <875343519@qq.com>
2020-11-27 23:43:21 +0800

    20201127

--- js/goodList.js
+++ js/goodList.js
@@ -10,6 +10,10 @@ $('.header').load('./common_header.html',function(){
             var goodsList = data.categoryItemList
              //遍历数据，生成样式结构 
             $.each(goodsList,function(index,item){
+                // if(index >= 2){
+                //     return
+                // }
+
                 var divStr = ''
                 // item.category.name是a标签
                 $('.allSort').append("<a href='#' class='sortA sameA'>"+item.category.name+"</a>")
@@ -19,8 +23,15 @@ $('.header').load('./common_header.html',function(){
                     // console.log(ind)
                     // console.log(val)
                     var price =val.counterPrice? ("<span class='prevPrice'><span class='doll'>￥</span>"+val.counterPrice+"</span>") :''
+                    var  counterPrice = val.counterPrice?val.counterPrice:''
+                    var obj = [{
+                                "data_name":val.name,
+                                "_src":val.listPicUrl,
+                                "nowP":val.retailPrice,
+                                "preP":counterPrice}]
+                    var jsonObj = JSON.stringify(obj)
                     if(!val.listPromBanner){
-                        var str = `<li class="item" data_id='${val.id}'>
+                        var str = `<li class="item" data_id= '${val.id}'   data='${jsonObj}' >
                         <div class="item_inner">
                             <div class="item_top">
                                 <div class="imgDiv">
@@ -47,7 +58,7 @@ $('.header').load('./common_header.html',function(){
                         `
                     
                     }else{
-                        var str = `<li class="item" data_id='${val.id}'>
+                        var str = `<li class="item" data_id= '${val.id}'   data='${jsonObj}' >
                         <div class="item_inner">
                             <div class="item_top">
                                 <div class="imgDiv">
@@ -112,16 +123,60 @@ $('.header').load('./common_header.html',function(){
             })
 
             // 点击跳转商品详情页
-            // $('.goodsList').on('click','li',function(){
-            //     window.open('./../goods.html')
-            // })
+            $('.goodsList').on('click','.item_inner',function(){
+                    window.open('./../goods.html')
+            })
             $('.goodsList').on('click','.addMyCar',function(){
-                alert('加入购物车')
-                
+                alert('成功加入购物车')
+                var parent = this.parentElement
+                var data_id = parent.getAttribute('data_id')
+                var data_massage = parent.getAttribute('data')
+                console.log(data_id)
+                console.log(data_massage)
+              
+                var good1 = [{
+                    "id":data_id,
+                    "data":data_massage,
+                    "num":1
+                    }]
+                var str = JSON.stringify(good1)
+                if(!localStorage.getItem('goods')){
+                    localStorage.setItem('goods',str)
+                }else{
+                    var goodObj = JSON.parse(localStorage.getItem('goods'));
+                    console.log(goodObj)
+                    var flag = true 
+                    var i = 0
+                   for (const key in goodObj) {
+                       console.log(key)
+                      var element = goodObj[key]
+                      console.log(element.id === data_id)
+                     if(element.id === data_id){
+                        flag = false //表示购物车有此商品
+                        i = key
+                     }
+                   }
+                   if(!flag){//表示购物车有此商品
+                    goodObj[i].num ++
+                    localStorage.setItem('goods',JSON.stringify(goodObj))
+                    }else{
+                    var goodNew = {
+                        "id":data_id,
+                        "data":data_massage,
+                        "num":1
+                        }
+                        goodObj.push(goodNew)
+                        console.log(goodObj)
+                        localStorage.setItem('goods',JSON.stringify(goodObj))
+
+                    }
+                }
               })
 
         }
     })
+
+
 });
 
 $('.footer').load('./common_footer.html');
--- js/myCar.js
+++ js/myCar.js
@@ -1,5 +1,65 @@
 $('.footer').load('./common_footer.html');
-$('.header').load('./common_header.html');
+$('.header').load('./common_header.html',function(){
+    if(!localStorage.getItem('goods')){
+        $('.myCar_inner').css('display','block')
+        $('.someCar_outer').css('display','none')
+    }else{
+        $('.myCar_inner').css('display','none')
+        $('.someCar_outer').css('display','block')
+        var allPrice = 0
+        var PrevllPrice = 0
+        var goodsObj = JSON.parse( localStorage.getItem('goods')) 
+        goodsObj.forEach(function(element,index) {
+            var dataObj = JSON.parse(element.data)[0]
+            var id = JSON.parse(element.id)
+            var {data_name,_src,nowP,preP} = dataObj
+            var totalPrice =nowP*element.num
+            var prevTotalPrice =preP*element.num
+            allPrice += totalPrice
+            PrevllPrice += prevTotalPrice
+            var str = `<li class="item_li clearfix" id='${id}' index ='${index}'>
+            <div class="itemdiv item1">
+                <input type="checkbox">
+            </div>
+            <div class="itemdiv item2">
+                <a href="#" class="itemImg"><img src="${_src}" ></a>
+            </div>
+            <div class="itemdiv item3">
+                <a href="#" class="introduce">${data_name}</a>
+                <p class="type"></p>
+            </div>
+            <div class="itemdiv item4">
+                ￥<span>${nowP}</span>
+            </div>
+            <div class="item5 itemdiv  ">
+               <div class="numDiv">
+                   <div class="deleteDeal">
+                       <i class="jian line"></i>
+                     </div>
+                     <input type="text" class="goodsNum" value="${element.num}">
+                     <div class="addDeal">
+                         <i class="line"></i>
+                         <i class="jia"></i>
+                     </div>
+               </div>
+           </div>
+           <div class="item6 itemdiv ">￥<span>${totalPrice}</span></div>
+           <div class="item7 itemdiv ">
+               <a href="#" class="addStar">移入收藏夹</a>
+               <a href="#" class="delete">删除</a>
+           </div>
+        </li>`
+            var li = $('<div>'+str+'</div>')[0]
+            console.log(li)
+            console.log(document.querySelector('.selected_ul'))
+            document.querySelector('.item_ul').appendChild(li)
+        });
+            var chajian = allPrice -PrevllPrice
+            $('.needPArice .money strong').text(allPrice)
+            $('.tot_price1 strong').text(PrevllPrice)
+            $('.tot_price2 strong').text(chajian)
+    }
+});
 $('.item_ul').on('click','.addDeal',function(){
     var val = $('.goodsNum').val()
     val++
@@ -22,3 +82,5 @@ $('.item_ul').on('click','.deleteDeal',function(){
         $('.deleteDeal').css('cursor','pointer')
     }
 })
+
+
